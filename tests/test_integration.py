00e866fc03cbbda248b9849ed7422024c440a4a4
coddy0717 <ccoelloa@unemi.edu.ec>
2026-07-12 10:17:56 -0500

    integracion test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -12,6 +12,6 @@ def test_registro_y_busqueda():
 def test_flujo_varios_contactos():
     agenda = Agenda()
     agenda.registrar_contacto("Juan Perez", "0991234567")
-    agenda.registrar_contacto("Ana Torres", "0987654321")
+    agenda.registrar_contacto("Ana Torres", "09876543211")
     assert agenda.total_contactos() == 2
     assert agenda.buscar_contacto("Ana Torres")["telefono"] == "0987654321"
